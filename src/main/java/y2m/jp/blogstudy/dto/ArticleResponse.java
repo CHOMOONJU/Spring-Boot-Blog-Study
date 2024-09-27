65b3e593bfc341df45487be7b4a97971909ed09d
CHOMOONJU <moonju.cho@y2m.jp>
2024-09-27 10:31:34 +0900

    key->id変更

--- src/main/java/y2m/jp/blogstudy/dto/ArticleResponse.java
+++ src/main/java/y2m/jp/blogstudy/dto/ArticleResponse.java
@@ -5,10 +5,12 @@ import y2m.jp.blogstudy.model.Article;
 
 @Getter
 public class ArticleResponse {
+    private Long id;
     private final String title;
     private final String content;
 
     public ArticleResponse(Article article) {
+        this.id = article.getId();
         this.title = article.getTitle();
         this.content = article.getContent();
     }
