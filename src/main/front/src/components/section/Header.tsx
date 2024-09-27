aa8b135e26c3dd7927f6e39241d46cb597744725
CHOMOONJU <moonju.cho@y2m.jp>
2024-09-27 14:04:14 +0900

    Section

---
+++ src/main/front/src/components/section/Header.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <header className='fixed top-0 w-full px-36 h-10'>
+       Blog
+    </header>
+  )
+}
+
+export default Header
---
+++ src/main/front/src/components/section/Main.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Main = (props: { children: string | number | boolean | React.ReactElement<any, string | React.JSXElementConstructor<any>> | Iterable<React.ReactNode> | React.ReactPortal | null | undefined }) => {
+  return (
+    <main className='px-36 pt-10'>
+      {props.children}
+    </main>
+  )
+}
+
+export default Main
